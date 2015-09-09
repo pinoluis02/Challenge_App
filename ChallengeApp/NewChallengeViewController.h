712a4424eaec35b5cdd8eb4d7fc39517042055f9
erickookie <yue.eyqt@gmail.com>
2015-09-09 01:29:54 -0400

    Working with the original

---
+++ ChallengeApp/NewChallengeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewChallengeViewController.h
+//  ChallengeApp
+//
+//  Created by MCS on 9/9/15.
+//  Copyright (c) 2015 MCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewChallengeViewController : UIViewController
+
+@end
