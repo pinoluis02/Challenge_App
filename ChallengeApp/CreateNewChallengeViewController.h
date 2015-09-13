04871077be0e7ec5677188acaa3113dc01fd9ff4
LuisRufino <pinoluis02@gmail.com>
2015-09-13 19:14:20 -0400

    Move sdksFb frameworks to framework group

--- ChallengeApp/CreateNewChallengeViewController.h
+++ ChallengeApp/CreateNewChallengeViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "FbSingleton.h"
 
 extern NSString * customCharityOrganizationOptionString;
 @interface CreateNewChallengeViewController : UIViewController<UIPickerViewDataSource, UIPickerViewDelegate>
